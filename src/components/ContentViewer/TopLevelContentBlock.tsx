3f02e6b8877d4192fc68623e36890066bc000db6
Leo Ware <leoware@minerva.kgi.edu>
2021-07-12 16:25:41 -0700

    annotation dots on scroll axis

--- src/components/ContentViewer/TopLevelContentBlock.tsx
+++ src/components/ContentViewer/TopLevelContentBlock.tsx
@@ -2,7 +2,10 @@ import React, {useRef} from "react"
 import {Set} from "immutable"
 import {ContentNodeType} from "../types"
 import ContentBlock from "../ContentBlock/ContentBlock"
-import {nodePrettyId} from "../functions";
+import {nodePrettyId} from "../functions"
+import {nodesInNode} from "../processing"
+
+import "./TopLevelContentBlock.css"
 
 type TopLevelContentBlockType = {
     node: ContentNodeType
@@ -17,18 +20,30 @@ const excludedContentTypes = Set([
 const TopLevelContentBlock: React.FC<TopLevelContentBlockType> = (props) => {
 
     const ref = useRef(null)
+    const numAnnotationsInKids = nodesInNode(props.node).reduce(
+        (c, n) => c.concat(n.props.annotations?
+            n.props.annotations.filter(a => !a._user).map(a => a._id) : Set()),
+        Set()
+    ).size
+
+    const selectMe = () => !excludedContentTypes.has(props.node.type) && props.setActiveNodeRef(ref)
 
     return (
         <div
             id={nodePrettyId(props.node)}
             className={"TopLevelContentBlock"}
             ref={ref}
-            style={{position: "relative", padding: "5px 0"}}
-
-            // question of taste which event to use here
-            onMouseUp={() => !excludedContentTypes.has(props.node.type) && props.setActiveNodeRef(ref)}
+            onMouseUp={selectMe}
         >
             <ContentBlock node={props.node} />
+            <div onClick={selectMe} className={"TopLevelContentBlock-dot-target"}>
+                {
+                    numAnnotationsInKids === 0? <></> :
+                        numAnnotationsInKids === 1? <div className={"TopLevelContentBlock-dot TopLevelContentBlock-dot-small"} /> :
+                            [2, 3,4].includes(numAnnotationsInKids)? <div className={"TopLevelContentBlock-dot TopLevelContentBlock-dot-med"} /> :
+                                <div className={"TopLevelContentBlock-dot TopLevelContentBlock-dot-big"} />
+                }
+            </div>
         </div>
     )
 }

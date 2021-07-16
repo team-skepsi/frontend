ad34fc522060c7c67f0a513f78173765214152e4
finnianmacken <finn@minerva.kgi.edu>
2021-07-16 09:11:34 -0700

    Created box with metadata average

--- src/components/ScoreMetadata/ScoreMetadata.js
+++ src/components/ScoreMetadata/ScoreMetadata.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import styles from "./ScoreMetadata.module.css"
+import { Card } from 'semantic-ui-react'
 
 function ScoreMetadata(props){
 
@@ -12,13 +14,16 @@ function ScoreMetadata(props){
         console.log(typeof fieldName)
         name = fieldName
         let sum = 0
+        let counter = 0
         for(let score of props.scores){
           if(fieldName===score.field){
             sum += score.score
+            counter += 1
         }
       }
       let obj = {}
-      obj[`${name}`]=sum
+      obj["field"] = name
+      obj["metadata"]=Number(sum/counter).toFixed(2)
       scoreArray.push(obj)
     }
 
@@ -35,7 +40,15 @@ function ScoreMetadata(props){
 
   return(
     <div>
-      <h1>Hi</h1>
+      <div className={styles.metadataWrapper}>
+        {sumFields.map(({field, metadata}, index)=>
+          <div key={index}>
+            <Card>
+                <p className={styles.metadataText}>{field}: {metadata}</p>
+            </Card>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
